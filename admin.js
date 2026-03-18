d4437af8491803c87040e1d2a614c315f2bfe92d
SCRIPTSROBLOX738833 <164623060+SCRIPTSROBLOX738833@users.noreply.github.com>
2026-03-18 06:50:45 +0200

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,35 @@
+import { db } from "./firebase-config.js";
+import {
+  ref,
+  onValue,
+  remove
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
+
+const scriptsContainer = document.getElementById("scriptsContainer");
+
+const scriptsRef = ref(db, "scripts");
+
+onValue(scriptsRef, (snapshot) => {
+  scriptsContainer.innerHTML = "";
+
+  snapshot.forEach((child) => {
+    const data = child.val();
+    const key = child.key;
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <h3>${data.title}</h3>
+      <pre>${data.code}</pre>
+      <p>Category: ${data.category}</p>
+      <button onclick="deleteScript('${key}')">حذف</button>
+      <hr>
+    `;
+
+    scriptsContainer.appendChild(div);
+  });
+});
+
+window.deleteScript = async function(key) {
+  await remove(ref(db, "scripts/" + key));
+  alert("تم الحذف");
+};
